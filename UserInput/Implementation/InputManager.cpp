d997a7fcece86a37d3f87038ad0ad975d6c3516a
Paul <paul.navin@gmail.com>
2020-01-01 00:19:00 +0000

    Changed mouse position toggle to right mouse button.

--- UserInput/Implementation/InputManager.cpp
+++ UserInput/Implementation/InputManager.cpp
@@ -13,10 +13,8 @@ InputManager::InputManager() {
     coreKeyMap_[ToggleFps] = new Command("Toggle FPS", toggleFpsBindings);
 
     std::vector<KeyBinding> toggleMousePositionBindings;
-    toggleMousePositionBindings.push_back(KeyBinding(VK_SHIFT, KeyState::StillPressed));
-    toggleMousePositionBindings.push_back(KeyBinding(VK_CONTROL, KeyState::StillPressed));
-    toggleMousePositionBindings.push_back(KeyBinding('M', KeyState::JustPressed));
-    coreKeyMap_[ToggleMousePosition] = new Command("Toggle FPS", toggleMousePositionBindings);
+    toggleMousePositionBindings.push_back(KeyBinding(VK_RBUTTON, KeyState::JustReleased));
+    coreKeyMap_[ToggleMousePosition] = new Command("Toggle Mouse Position", toggleMousePositionBindings);
 
     std::vector<KeyBinding> quitBindings;
     quitBindings.push_back(KeyBinding(VK_ESCAPE, KeyState::JustPressed));
