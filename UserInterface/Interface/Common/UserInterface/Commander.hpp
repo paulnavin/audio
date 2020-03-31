56d338ce2a1f6a12c874f3dfc53944e1cd1edb50
Paul Navin <paul.navin@gmail.com>
2020-03-31 18:16:28 +0100

    Added skeleton Commander and ModelPortal.

---
+++ UserInterface/Interface/Common/UserInterface/Commander.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <ErrorHandling/Result.hpp>
+#include <UserInput/Command.hpp>
+
+class Element;
+
+class Commander {
+
+public:
+    Commander() = default;
+    virtual ~Commander() = default;
+
+public:
+    Result Init();
+
+public:
+    void RegisterForCommand(Command::Id command, Element* receiver);
+
+private:
+    std::map<Command::Id, std::vector<Element*> > receivers_;
+};
---
+++ UserInterface/Interface/Common/UserInterface/ModelPortal.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class Commander;
+class GraphicsEngine;
+
+struct ModelPortal {
+    GraphicsEngine* gfx;
+    Commander* commander;
+};
