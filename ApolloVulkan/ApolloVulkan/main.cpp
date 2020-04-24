feb30c44833ff20ae004dc1a3ca2192b42acd0bd
Paul <paul.navin@gmail.com>
2020-04-24 19:27:51 +0100

    Vulkan tutorial, after creating instance.

--- ApolloVulkan/ApolloVulkan/main.cpp
+++ ApolloVulkan/ApolloVulkan/main.cpp
@@ -1,4 +1,5 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -8,23 +9,73 @@
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
     
 private:
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkypoos", nullptr, nullptr);
+    }
+
     void initVulkan() {
-        
+        createInstance();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Triangle of Doom";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "Engine of Doom";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &vkInstance);
+
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
     
     void mainLoop() {
-        
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
     
     void cleanup() {
-        
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
     }
+
+    GLFWwindow* window;
+    VkInstance vkInstance;
 };
 
 int main() {
